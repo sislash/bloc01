6def3b8e80929fbc62c3b7bb2b31ddc74a75190b
sislash <sckavens@gmail.com>
2025-07-11 14:56:52 +0200

    modiffier petit erreur dans l'ex00

--- ex00/test/main.c
+++ ex00/test/main.c
@@ -5,11 +5,9 @@
 /* By: Xavier.Megnoux <sckavens@gmail.com>                                    */
 /*                                                                            */
 /* Created: 2025/05/06 20:41:00 by Xavier.Megnoux                             */
-/* Updated: 2025/06/11 16:25:12 by Xavier.Megnoux                             */
+/* Updated: 2025/07/11 14:56:08 by Xavier.Megnoux                             */
 /*                                                                            */
 /******************************************************************************/
-#include "/home/sckavens/Piscine_C/bloc01/ex00/include/ft_putchar.h"
-
 void ft_putchar(char c);
 
 int main()
